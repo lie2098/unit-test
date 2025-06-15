c27bcfdfee9ae72f2e340392619a93dd01da9028
Lye <lyrollorenzo@gmail.com>
2025-06-16 01:04:23 +0800

    -added db script
    -update application properties
    -update db connection
    -update source propery for unit test

--- 5-starting-project/src/test/java/com/lye/springmvc/service/StudentAndGradeServiceTest.java
+++ 5-starting-project/src/test/java/com/lye/springmvc/service/StudentAndGradeServiceTest.java
@@ -21,7 +21,7 @@ import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-@TestPropertySource("/application.properties")
+@TestPropertySource("/application-test.properties")
 @SpringBootTest
 public class StudentAndGradeServiceTest {
 
